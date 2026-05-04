83665aba997ac0c7bd606399da2c4dbf884c84c3
LIBIA71421 <baka65311131@gmail.com>
2026-05-04 18:06:51 -0400

    test(web-ui): fix cypress landing spec to visit root path instead of /landing

--- Web Ui/test/cypress/e2e/landing.spec.ts
+++ Web Ui/test/cypress/e2e/landing.spec.ts
@@ -1,6 +1,6 @@
 describe("Landing page", () => {
   it("is publicly accessible and exposes the authentication flow", () => {
-    cy.visit("/landing");
+    cy.visit("/");
 
     cy.contains("Todo para crear software").should("be.visible");
     cy.contains("button", "Comienza ahora").scrollIntoView().should("be.visible").click();
